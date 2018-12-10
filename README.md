d1113b0a4755a241decc33cc9f36d2cf10c4c7bf
outprog <xiongwei_13@hotmail.com>
2018-12-10 16:10:34 +0800

    updated cli.go & README

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # ether-abi
 A cli tool for decode &amp; encode Ethereum ABI
+
+## Installation
+
+```
+go get github.com/outprog/ether-abi
+```
+
+## Examples
+
+ABI must json file.
+
+- abi.json
+
+```
+[
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "wad",
+                "type": "uint256"
+            }
+        ],
+        "name": "withdraw",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
+```
+
+#### encode
+
+```
+ether-abi --file abi.json --type encode --method withdraw --args 10000
+# 0x2e1a7d4d0000000000000000000000000000000000000000000000000000000000002710
+```
+
+#### decode
+
+## Development
+
+```
+go get -d github.com/outprog/ether-abi
+cd $GOPATH/src/github.com/outprog/ether-abi
+sh dep.sh
+```
+
+Run example:
+
+```
+go run ./cli.go --file abi.example.json --method withdraw --args 10000
+# 0x2e1a7d4d0000000000000000000000000000000000000000000000000000000000002710
+```
+
--- cli.go
+++ cli.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 
@@ -14,32 +15,42 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "ether-abi"
 	app.Flags = []cli.Flag{
-		cli.StringFlag{Name: "file", EnvVar: "FILE", Usage: "load your json file(abi)"},
-		cli.StringFlag{Name: "type", EnvVar: "TYPE", Value: "encode", Usage: "encode or decode, default encode"},
-		cli.StringFlag{Name: "method", EnvVar: "METHOD", Usage: "method name you want to encode or decode"},
-		cli.StringFlag{Name: "args", EnvVar: "ARGS", Usage: "args of method"},
+		cli.StringFlag{Name: "file, f", Usage: "load abi(json) from file"},
+		cli.StringFlag{Name: "type, t", Value: "encode", Usage: "encode or decode"},
+		cli.StringFlag{Name: "method, m", Usage: "method name for encode or decode"},
+		cli.StringFlag{Name: "args, a", Usage: "args of method, split of comma(,)"},
 	}
 	app.Action = run
 	err := app.Run(os.Args)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
 
 func run(c *cli.Context) (err error) {
-	json, err := ioutil.ReadFile(c.String("file"))
+	cfile := c.String("file")
+	ctype := c.String("type")
+	cmethod := c.String("method")
+	cargs := c.String("args")
+	if cfile == "" || ctype == "" || cmethod == "" {
+		fmt.Print("wrong arguments\nusage: ether-abi -h")
+		return
+	}
+
+	json, err := ioutil.ReadFile(cfile)
 	if err != nil {
 		return err
 	}
 	wabi := utils.NewWABI(string(json))
 
-	aArgs := strings.Split(c.String("args"), ",")
+	aArgs := strings.Split(cargs, ",")
 
 	output := ""
-	if c.String("type") == "decode" {
+	if ctype == "decode" {
 		// output, err = wabi.Decode()
 	} else {
-		output, err = wabi.Encode(c.String("method"), aArgs...)
+		output, err = wabi.Encode(cmethod, aArgs...)
+		output = "0x" + output
 	}
 	if err != nil {
 		return err
