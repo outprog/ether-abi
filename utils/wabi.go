a14333a8b1af3a2562decca38e19dd2a3c626511
outprog <xiongwei_13@hotmail.com>
2019-03-07 18:08:42 +0800

    added array encode

--- utils/wabi.go
+++ utils/wabi.go
@@ -36,6 +36,14 @@ func (w *WABI) Encode(method string, args ...string) (string, error) {
 			iArgs = append(iArgs, common.HexToAddress(v))
 		case "uint256":
 			iArgs = append(iArgs, math.MustParseBig256(v))
+		case "address[]":
+			addrs := []common.Address{}
+			value := strings.Replace(strings.Replace(v, "[", "", 1), "]", "", 1)
+			values := strings.Split(value, ".")
+			for _, vv := range values {
+				addrs = append(addrs, common.HexToAddress(vv))
+			}
+			iArgs = append(iArgs, addrs)
 		default:
 			return "", fmt.Errorf("unsupported arg type")
 		}
